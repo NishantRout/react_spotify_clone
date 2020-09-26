a82c6ee75d60e9800f09eb7099465d11a43aa46c
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-27 01:38:16 +0530

    sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,3 +1,4 @@
+import { Home, LibraryMusic, Search } from '@material-ui/icons';
 import React from 'react';
 import '../css/Sidebar.css';
 import SidebarOption from './SidebarOption';
@@ -10,9 +11,12 @@ function Sidebar() {
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
                 alt="spotify sidebar logo"
             />
-            <SidebarOption title="Home" />
-            <SidebarOption title="Search" />
-            <SidebarOption title="Your Library" />
+            <SidebarOption Icon={Home} title="Home" />
+            <SidebarOption Icon={Search} title="Search" />
+            <SidebarOption Icon={LibraryMusic} title="Your Library" />
+            <br />
+            <strong className="sidebar__title">PLAYLISTS</strong>
+            <hr />
         </div>
     )
 }
